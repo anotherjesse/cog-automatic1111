89f143fc431a8fc81f6b778c86a889720d5c1d17
Jesse Andrews <jesse@replicate.com>
2023-06-01 00:50:01 +0000

    first steps?

--- predict.py
+++ predict.py
@@ -3,27 +3,42 @@ import shutil
 from typing import Iterator
 
 from cog import BasePredictor, Input, Path
+import random
 import subprocess
+import time
+import requests
+
+import webuiapi
 
 
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         self.port = 7860
+        # fixme(ja): this script probably is doing too much, we can probably just skip directly to launching?
         self.daemon = subprocess.Popen(["./webui.sh", "--listen", "--port", str(self.port), "--api", "--no-download-sd-model"], cwd="./stable-diffusion-webui")
+
+        # Wait for the server to start
+        start = time.time()
+        while True:
+            try:
+                requests.get(f"http://localhost:{self.port}/")
+                break
+            except requests.exceptions.ConnectionError:
+                if time.time() - start > 60:
+                    raise RuntimeError("Server failed to start")
+                time.sleep(0.1)
+
+        print("startup complete, duration:", time.time() - start)
+
+        # FIXME(ja): seems like you need to let it sleep a little more after it starts listening on the port
+        time.sleep(5)
+
+        self.api = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
+
 
     def predict(
         self,
-        control_image: Path = Input(
-            description="Optional Image to use for guidance based on canny",
-            default=None,
-        ),
-        image: Path = Input(
-            description="Optional Image to use for img2img guidance", default=None
-        ),
-        mask: Path = Input(
-            description="Optional Mask to use for legacy inpainting", default=None
-        ),
         prompt: str = Input(
             description="Input prompt",
             default="photo of cjw person",
@@ -32,63 +47,21 @@ class Predictor(BasePredictor):
             description="Specify things to not see in the output",
             default=None,
         ),
-        width: int = Input(
-            description="Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=512,
-        ),
-        height: int = Input(
-            description="Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=512,
-        ),
-        num_outputs: int = Input(
-            description="Number of images to output.",
-            ge=1,
-            le=10,
-            default=1,
-        ),
-        num_inference_steps: int = Input(
+        steps: int = Input(
             description="Number of denoising steps", ge=1, le=500, default=50
         ),
-        guidance_scale: float = Input(
+        cfg_scale: float = Input(
             description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
         ),
-        prompt_strength: float = Input(
-            description="Prompt strength when using init image. 1.0 corresponds to full destruction of information in init image",
-            default=0.8,
-        ),
-        low_threshold: int = Input(
-            description="Low threshold for canny filter", default=100
-        ),
-        high_threshold: int = Input(
-            description="High threshold for canny filter", default=200
-        ),
-        scheduler: str = Input(
-            default="DPMSolverMultistep",
-            choices=[
-                "DDIM",
-                "DPMSolverMultistep",
-                "HeunDiscrete",
-                "K_EULER_ANCESTRAL",
-                "K_EULER",
-                "KLMS",
-                "PNDM",
-                "UniPCMultistep",
-            ],
-            description="Choose a scheduler.",
-        ),
-        disable_safety_check: bool = Input(
-            description="Disable safety check. Use at your own risk!", default=False
-        ),
-        return_processed_control: bool = Input(
-            description="if using control_image, return processed control as first image", default=False
-        ),
+        
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed", default=None
         ),
     ) -> Iterator[Path]:
         """Run a single prediction on the model"""
+
+        if not seed:
+            seed = random.randint(0, 2 ** 32 - 1)
 
         status = self.daemon.poll()
 
@@ -101,3 +74,13 @@ class Predictor(BasePredictor):
         else:
             print("Process terminated with error. Exit code:", status)
 
+        result = self.api.txt2img(prompt=prompt,
+                            negative_prompt=negative_prompt,
+                            seed=seed,
+                            # styles=["anime"],
+                            cfg_scale=cfg_scale,
+                            steps=steps)
+
+        for idx, image in enumerate(result.images):
+            image.save(f"output_{idx}.png")
+            yield Path(f"output_{idx}.png")
